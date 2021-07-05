b1b1cec244ae9bdb9467352909f1267be4ac6b62
Rithik Kumar <rithikkumar31299@gmail.com>
2021-07-05 14:20:45 +0000

    bdhvaskvd

--- src/main/java/com/asde/dbconnect/service/SlotService.java
+++ src/main/java/com/asde/dbconnect/service/SlotService.java
@@ -21,21 +21,21 @@ public class SlotService implements ISlotService {
      * Get all available slots that are present in the file as a list of objects 
      * containing the location for each slot as well
      */
-    public List<VaccineSlot> getSlotsPresentInFile(String filename){
-        // FileReadingServiceImpl read = new FileReadingServiceImpl();
-        // int slotsForFirst = read.getTotalSlotsForLocation("gurugram", "covaxin");
-        List<VaccineSlot> slot = new ArrayList<>();
-        FileReadingServiceImpl read = new FileReadingServiceImpl();
-        slot = read.readVaccinCentreData(filename);
+    // public List<VaccineSlot> getSlotsPresentInFile(String filename){
+    //     // FileReadingServiceImpl read = new FileReadingServiceImpl();
+    //     // int slotsForFirst = read.getTotalSlotsForLocation("gurugram", "covaxin");
+    //     List<VaccineSlot> slot = new ArrayList<>();
+    //     FileReadingServiceImpl read = new FileReadingServiceImpl();
+    //     slot = read.readVaccinCentreData(filename);
 
-        return slot;
+    //     return slot;
 
 
-    }
+    // }
     @Override
     public List<VaccineSlot> getAllAvailableSlots() {
         List<VaccineSlot> slots = new ArrayList<VaccineSlot>();
-        slots.add(null);
+        slots=frs.readVaccinCentreData(fileName);
         return slots;
     }
 
@@ -48,9 +48,15 @@ public class SlotService implements ISlotService {
     @Override
     public Integer getVaccineSlotsForLocationAndVaccine(String location, String vaccine_name) {
         // TODO Auto-generated method stub
-        FileReadingServiceImpl read = new FileReadingServiceImpl();
-        int ans=read.getTotalSlotsForLocation(location, vaccine_name);
-        return ans;
+        int t=0;
+        List<VaccineSlot> slots = new ArrayList<>();
+        slots=frs.readVaccinCentreData(fileName);
+        for(VaccineSlot v:slots){
+            if(v.getLocation_name()==location && v.getVaccine()==vaccine_name){
+                t=t+v.getStock();
+            }
+        }
+        return t;
     }
 
     /**
@@ -61,8 +67,13 @@ public class SlotService implements ISlotService {
     @Override
     public List<VaccineSlot> getVaccineSlotsForLocation(String location) {
     	List<VaccineSlot> slots = new ArrayList<VaccineSlot>();
-        FileReadingServiceImpl read = new FileReadingServiceImpl();
-        slots = read.readVaccinCentreData(location);
+        List<VaccineSlot> slots1 = new ArrayList<VaccineSlot>();
+
+        slots1 = frs.readVaccinCentreData(this.fileName);
+
+        for(VaccineSlot v : slots1){
+            slots.add(v);
+        }
 
         return slots;
     }
--- src/main/java/com/asde/file/FileReadingServiceImpl.java
+++ src/main/java/com/asde/file/FileReadingServiceImpl.java
@@ -1,46 +1,51 @@
 package com.asde.file;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
 
+//import com.asde.dbconnect.entities.Vaccine;
 import com.asde.dbconnect.entities.VaccineSlot;
 
-import org.apache.avro.file.FileReader;
+//import org.apache.avro.file.FileReader;
 
-import au.com.bytecode.opencsv.CSVReader;
+//import au.com.bytecode.opencsv.CSVReader;
 import structures.ArrayList;
 
 public class FileReadingServiceImpl implements IFileReadingService {
 
-    @Override
-    public Integer getTotalSlotsForLocation(String location, String vaccineType) {
-        try (CSVReader reader = new CSVReader((Reader) new FileReader(location))) {
-            List<String[]> r = reader.readAll();
-            int t=0;
+     @Override
+     public Integer getTotalSlotsForLocation(String location, String vaccineType) {
+         
+    //     try (CSVReader reader = new CSVReader((Reader) new FileReader(location))) {
+    //         List<String[]> r = reader.readAll();
+    //         int t=0;
 
-                String[] first = Arrays.copyOf(r[1], 4);
-                t=Integer.parseInt(first[3]);
-                if(vaccineType==first[0]&&location==first[1]){
-                    return t;
-                }
+    //             String[] first = Arrays.copyOf(r[1], 4);
+    //             t=Integer.parseInt(first[3]);
+    //             if(vaccineType==first[0]&&location==first[1]){
+    //                 return t;
+    //             }
 
-                String[] second = Arrays.copyOf(r[2],4);
-                t=Integer.parseInt(second[3]);
-                if(vaccineType==second[0]&&location==second[1]){
-                    return t;
-                }
+    //             String[] second = Arrays.copyOf(r[2],4);
+    //             t=Integer.parseInt(second[3]);
+    //             if(vaccineType==second[0]&&location==second[1]){
+    //                 return t;
+    //             }
 
-                String[] third = Arrays.copyOf(r[3],4);
-                t=Integer.parseInt(third[3]);
-                if(vaccineType==third[3]&&location==third[1]){
-                    return t;
+    //             String[] third = Arrays.copyOf(r[3],4);
+    //             t=Integer.parseInt(third[3]);
+    //             if(vaccineType==third[3]&&location==third[1]){
+    //                 return t;
 
 
-            }
-            return null;
-        }
+    //         }
+    //         return null;
+    //     }
+        return null;
 	
 	}
 
@@ -48,48 +53,36 @@ public class FileReadingServiceImpl implements IFileReadingService {
 	public List<VaccineSlot> readVaccinCentreData(String filename) {
         List<VaccineSlot> vaccineSlot = new ArrayList<>();
 
-        try(CSVReader reader = new CSVReader((Reader) new FileReader(filename))) {
-         List<String[]> r = reader.readAll();
-            int t=0;
-            boolean b = true;
-            
-                String[] s=r[1];
-                String[] first = Arrays.copyOf(r[1],4);
-                VaccineSlot vs1 = new VaccineSlot();
-                t=Integer.parseInt(first[3]);
-                vs1.setStock(t);
-                vs1.setLocation_name(first[1]);
-                vs1.setVaccine(first[0]);
-                b=Boolean.parseBoolean(first[2]);
-                vs1.setIs_available(b);
-                vaccineSlot.add(vs1);
+          
+        try{
+            BufferedReader buffer = new BufferedReader((Reader) new FileReader(filename));
+            List<List<String>> data = new ArrayList<>();
+            String delimeter = ",";
+            String line;
+            while((line=buffer.readLine())!=null){
+                String[] values = line.split(delimeter);
+                data.add(Arrays.asList(values));
+            }
+            for(int i=1;i<data.size();i++){
+                VaccineSlot vs = new VaccineSlot();
+                vs.setVaccine(data.get(i).get(0));
+                vs.setLocation_name(data.get(i).get(1));
+                boolean check = Boolean.parseBoolean(data.get(i).get(2));
+                vs.setIs_available(check);
+                int s=Integer.parseInt(data.get(i).get(3));
+                vs.setStock(s);
+                vaccineSlot.add(vs);
+            }
 
-                String[] second = Arrays.copyOf(r[2],4);
-                VaccineSlot vs2 = new VaccineSlot();
-                t=Integer.parseInt(second[3]);
-                vs2.setStock(t);
-                vs2.setLocation_name(second[1]);
-                vs2.setVaccine(second[0]);
-                b=Boolean.parseBoolean(second[2]);
-                vs2.setIs_available(b);
-                vaccineSlot.add(vs2);
-
-                String[] third = Arrays.copyOf(r[3],4);
-                VaccineSlot vs3 = new VaccineSlot();
-                t=Integer.parseInt(third[3]);
-                vs3.setStock(t);
-                vs3.setLocation_name(third[1]);
-                vs3.setVaccine(third[0]);
-                b=Boolean.parseBoolean(third[2]);
-                vs3.setIs_available(b);
-                vaccineSlot.add(vs3);
-                
-
-                return vaccineSlot;
-        }  
+        }
+        catch(IOException e){
+            System.out.println("error");
+        }
+        return vaccineSlot;
 
 	}
 
-   
+    
+    
 
 }
